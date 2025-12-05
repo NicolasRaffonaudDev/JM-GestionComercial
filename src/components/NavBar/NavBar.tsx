fbbff2944dd751b95ba04102f956766dbbbb3119
Nicolas Raffonaud <nicolasraffonaudsoft@gmail.com>
2025-12-05 00:25:28 -0300

    configuraciones iniciales, feat: implementacion de NavBar con menu hamburguesa para mobile y hero en home

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const NavBar: React.FC = () => {
+  const [isOpen, setIsOpen]= useState(false);   
+  return (
+    <nav className="bg-primary-green p-4 text-white">
+    <div className="flex justify-between items-center">
+      <Link to="/">JM Gestión</Link>
+      <button onClick={() => setIsOpen(!isOpen)} className="md:hidden">☰</button> {/* Hamburguesa */}
+      <div className={`md:flex space-x-4 ${isOpen ? 'block' : 'hidden'}`}> {/* Toggle */}
+        <Link to="/lotes">Lotes</Link>
+        <Link to="/contacto">Contacto</Link>
+      </div>
+    </div>
+  </nav>
+  );
+};
+
+export default NavBar
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+const Home: React.FC = () => {
+  return (
+    <div>
+      <div className="bg-earth-brown text-white p-8 text-center"> {/* Hero custom */}
+        <h1 className="text-4xl">Gestión Comercial de Lotes</h1>
+        <p className="text-xl">Encuentra tu terreno ideal en Argentina. Explora ahora!</p>
+        <button className="bg-primary-green px-4 py-2 mt-4">Ver Lotes</button>
+      </div>
+      {/* Agrega más sections */}
+    </div>
+  );
+};
+
+export default Home;
